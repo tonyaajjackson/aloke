bc9035891c121e3f838d0ce987c6ed86198a53aa
Tony Jackson <tony.aaj.jackson@gmail.com>
2018-10-13 16:56:05 -0700

    Initial commmit

---
+++ word_gen.py
@@ -0,0 +1,40 @@
+#! usr/bin/ python3
+
+import numpy
+import random
+
+prob = numpy.load("prob.npy")
+
+num_names = int(input("How many names do you want to generate?"))
+
+for x in range(num_names):
+    end_of_word = False
+    prev_letter = 0
+    word = ""
+
+    # Add loop counter to catch if loop gets stuck
+    loops = 0
+    while not end_of_word:
+        rand_prob = random.random()
+
+        # Find a letter corresponding to this probability
+        found_letter = False
+        current_letter = 0
+        while not found_letter:
+            if rand_prob <= prob[prev_letter, current_letter]:
+                found_letter = True
+                prev_letter = current_letter
+            else:
+                current_letter +=1
+
+        if current_letter == 0:
+            end_of_word = True
+        else:
+            word += chr(current_letter+96)
+
+        loops +=1
+        if loops > 100:
+            print("Looped too many times - exiting")
+            end_of_word = True
+
+    print(word)
